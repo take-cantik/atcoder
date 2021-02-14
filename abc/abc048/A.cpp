d9df7128fd2414f7f45c34cff29e7cad5ddaa12a
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-02-14 10:33:16 +0900

    AtCoder***Contest

---
+++ abc/abc048/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  string s1, s2, s3;
+  cin >> s1 >> s2 >> s3;
+
+  cout << s1.at(0) << s2.at(0) << s3.at(0) << endl;
+
+  return 0;
+}
+
