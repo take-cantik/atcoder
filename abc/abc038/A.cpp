c6e3f3ce558a689688196b434ef129918215f643
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-02-14 10:51:05 +0900

    TEA

---
+++ abc/abc038/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  string S;
+  cin >> S;
+
+  if (S.at(S.size() - 1) == 'T') {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+
+  return 0;
+}
+
