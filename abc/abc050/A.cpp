573995ebf5691c2fd528a3d0f7cac5caa152cd71
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-02-14 10:37:26 +0900

    Addition and Subtraction Easy

---
+++ abc/abc050/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  int A, B;
+  char op;
+
+  cin >> A >> op >> B;
+
+  if (op == '+') {
+    cout << A + B << endl;
+  } else {
+    cout << A - B << endl;
+  }
+
+  return 0;
+}
+
