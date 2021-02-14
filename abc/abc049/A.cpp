26356b40d08d3342734d09109c5e6b92984aec59
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-02-14 10:27:08 +0900

    Finish the Iai and cover the blue picture

---
+++ abc/abc049/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  char c;
+  cin >> c;
+
+  if (c == 'a' || c == 'i' || c == 'u' || c == 'e' || c == 'o') {
+    cout << "vowel" << endl;
+  } else {
+    cout << "consonant" << endl;
+  }
+
+  return 0;
+}
+
